6faa8bfb1143284c73cb6c5eb0a4e1a89e559081
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-08 17:44:41 -0500

    C Input Output (I/O)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main() {
-    printf("Hello World!");
+int main1() {
+    //printf("Hello World!");
     return 0;
 }
 
@@ -33,5 +33,54 @@ int datatypes() {
     long double d; // holds 16 bytes
     unsigned int x = 35; //cannot hold negative values
     int y = 36; //can hold both positive and negative values
+}
 
+int main() {
+
+    int testint = 5;
+    float number1 = 13.5;
+    double number2 = 12.4;
+    char chr = 'a';
+    int testInteger;
+    float num1;
+    double num2;
+    char character;
+
+    printf("Output example");//Regular output example
+    printf("number1 = %f\n", number1);//float output example
+    printf("number2 = %lf", number2);//double output example
+    printf("testint = %d\n", testint);//Integer output example
+    printf("character = %c", chr);  //character output example
+    //integer in/out example
+    printf("Enter an integer: ");
+    scanf("%d", &testInteger);
+    printf("Number = %d",testInteger);
+    //float and double in/out example
+    printf("Enter a number: ");
+    scanf("%f", &num1);
+    printf("Enter another number: ");
+    scanf("%lf", &num2);
+
+    printf("num1 = %f\n", num1);
+    printf("num2 = %lf", num2);
+    //character in/out example
+    printf("Enter a character: ");
+    scanf("%c",&character);
+    printf("You entered %c.", character);
+
+    //ASCII in/out example
+    // When %d is used, ASCII value is displayed
+    printf("ASCII value is %d.", character);
+
+    //multible values in/out example
+    int a;
+    float b;
+
+    printf("Enter integer and then a float: ");
+
+    // Taking multiple inputs
+    scanf("%d%f", &a, &b);
+
+    printf("You entered %d and %f", a, b);
+    return 0;
 }
