1357c4d50acda07aeb10196f2988227b848ee1d3
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-08 13:16:49 -0500

    C variables, constants, and literals

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
-
+#include <stdio.h>
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    printf("Hello World!");
     return 0;
 }
+
+int variables() {
+    int age = 25;
+    char ch = 'a';
+    ch = '1';
+}
+
+int constants() {
+    const double pi = 3.14;
+    // PI is a symbolic constant so it cannot be changed
+}
+
+int literals() {
+    //A literal is anything in ''
+}
+
