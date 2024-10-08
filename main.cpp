0fb7a7020bd388d02d1e23cc89331a4349ec8deb
y33t3r3r33 <dumasdd9@gmail.com>
2024-10-08 13:38:17 -0500

    C data types

--- main.cpp
+++ main.cpp
@@ -19,3 +19,19 @@ int literals() {
     //A literal is anything in ''
 }
 
+int datatypes() {
+    int id; //Can declare multiple variables at once useing a comma ex: int id, age;
+    float salary; //float variable
+    double price; //double variable
+    //both float and double hold numbers
+    //the diffrence is float holds 4 bytes and a double holds 8 bytes
+    char test = 'h'; //character variable
+    //void datatypes returns to nothing and it cannot be used as a variable
+    short a; //Holds 2 bytes
+    long b; // holds 8 bytes
+    long long c; //holds 8 bytes
+    long double d; // holds 16 bytes
+    unsigned int x = 35; //cannot hold negative values
+    int y = 36; //can hold both positive and negative values
+
+}
